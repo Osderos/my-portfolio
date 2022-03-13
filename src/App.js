87d307eff0603bb10df628235c6cb103afa67840
Osderos <barda.george@gmail.com>
2022-03-13 19:06:20 +0200

    skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Image from "./dummyPortrait.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <HomeWrapper>
+      <PortraitImage src={Image} alt="myPicture"></PortraitImage>
+      <AboutMe>
+        <h1>Greetings! <br/>I'm George Barda</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am currently a mechanical engineer with over 7 years of experience
+          in the automotive industry. Being passionate about design and tech in
+          general and always opened to learn new skills and abilities , I have
+          decided approaching the Web Development area. Commited and aplying the
+          ideea of life-long learning, I am also a web developper with a passion
+          for JavaScript, React and all things in web development.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button>To portfolio</button>
+      </AboutMe>
+    </HomeWrapper>
   );
 }
 
 export default App;
+
+const HomeWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to right, rgb(42, 13, 40), rgb(6, 13, 40));
+  color: white;
+  width: 50%;
+  margin: auto;
+  padding: 4em;
+`;
+
+const PortraitImage = styled.img`
+  max-width: 25em;
+  box-shadow: none;
+`;
+
+const AboutMe = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
